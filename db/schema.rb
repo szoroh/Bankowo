f96da9a44ce30ed383229ea71cb9811bc450ee95
szoroh <kubahorosz95@gmail.com>
2019-10-01 22:38:23 +0200

    Add bank account to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_140939) do
+ActiveRecord::Schema.define(version: 2019_09_28_154613) do
+
+  create_table "bank_accounts", force: :cascade do |t|
+    t.integer "bank_accounts_id"
+    t.integer "user_id"
+    t.string "iban", null: false
+    t.string "currency", null: false
+    t.decimal "balance"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["bank_accounts_id"], name: "index_bank_accounts_on_bank_accounts_id"
+    t.index ["user_id"], name: "index_bank_accounts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -30,4 +42,6 @@ ActiveRecord::Schema.define(version: 2019_09_21_140939) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bank_accounts", "bank_accounts", column: "bank_accounts_id"
+  add_foreign_key "bank_accounts", "users"
 end
