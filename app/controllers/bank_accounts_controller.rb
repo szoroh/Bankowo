22fba508c716c3cee75fc4c21d5966ed42a883b2
szoroh <kubahorosz95@gmail.com>
2019-10-02 15:23:11 +0200

    Add new and create actions for bank account

---
+++ app/controllers/bank_accounts_controller.rb
@@ -0,0 +1,23 @@
+class BankAccountsController < ApplicationController
+  def new
+    @bank_account = BankAccount.new
+  end
+
+  def create
+    @bank_account = current_user.bank_accounts.new(bank_account_params)
+
+    if @bank_account.save
+      flash[:success] = 'Account successfully created.'
+      redirect_to(@bank_account)
+    else
+      flash[:error] = @bank_account.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def bank_account_params
+    params.require(:bank_account).permit(:iban, :balance, :currency)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :bank_accounts, only: [:new, :create]
   devise_for :users,
     controllers: { registrations: 'registrations' }
 end
